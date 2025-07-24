3adc6ebafcb0006530a53aa454d54cb5e404bc88
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-07-24 11:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const p = document.getElementById("text");
+const speed = document.getElementById("speed");
+let speedPerChar = 500 / parseInt(speed.value);
+let text = "We Love Programming!";
+
+speed.addEventListener("change", (e) => {
+	p.textContent = "";
+	if(!speed) return;
+	let i = 0;
+	let interval = setInterval(() => {
+		p.textContent += text[i];
+		i++;
+		if(i === text.length){
+			clearInterval(interval);
+		}
+	}, speedPerChar);
+	speed.value = "";
+})
